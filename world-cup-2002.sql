494a6c35c9a858a078564c7631cf119896e5198c
June Ekpo <jekpo@users.noreply.github.com>
2014-06-25 19:26:57 -0400

    Create world-cup-2002.sql

---
+++ world-cup-2002.sql
@@ -0,0 +1,24 @@
+--- Country Codes
+--- United States = USA
+--- Canada = CAN
+--- Mexico = MEX
+--- Brasil = BRA
+--- Italy  = ITA
+--- Nigeria = NGA
+
+
+--- INSERT Queries
+
+INSERT INTO `players` (`fname`, `lname`, `age`, `country`, `weight`) VALUES 
+('June', 'Ekpo', 31, 'USA', 150.85),
+('Joshua', 'Ekpo', 6, 'USA', 50.99),
+('Angel', 'Ekpo', 0, 'USA', 8.55);
+
+--- UPDATE Queries
+
+UPDATE `players` SET `active` = 1 WHERE `country` = 'CAN';
+UPDATE `players` SET `age` = 18 WHERE `country` = 'ITA';
+
+--- DELETE Queries
+DELETE FROM `players` WHERE `active` = 1 AND `country` = 'BRA';
+DELETE FROM `players` WHERE `active` = 1 AND `country` = 'MEX';
